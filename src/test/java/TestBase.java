14fd164fe20095d253c0b1cb77f1f5c9d13f9d4a
Veronika Iatckaia <neznatnov@gmail.com>
2023-04-09 16:39:24 +0200

    spaces and syntax changes

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,19 +1,8 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-
-
 public class TestBase {
     @BeforeAll
     static void BeforeAll(){
         Configuration.browser = "chrome";
-
-
-
-
-
     }
-
-
-
 }
--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,26 +1,23 @@
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-
 public class TestForm extends TestBase {
     @Test
-    void FirstTest() throws InterruptedException {
+    void FirstTest() {
         open("https://demoqa.com/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
-
-        $("[id=firstName]").setValue("Veronika");
-        $("[id=lastName]").setValue("Iatckaia");
-        $("[id=userEmail]").setValue("neznatnov@gmail.com");
+        $("#firstName").setValue("Veronika");
+        $("#lastName").setValue("Iatckaia");
+        $("#userEmail").setValue("neznatnov@gmail.com");
         $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("0123456789");
         $("#dateOfBirthInput").click();
-        $(byClassName("react-datepicker__month-select")).selectOptionByValue(String.valueOf(3));
-        $(byClassName("react-datepicker__year-select")).selectOptionByValue(String.valueOf(1995));
-        $(byClassName("react-datepicker__month")).$(byText("17")).click();
+        $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(3));
+        $(".react-datepicker__year-select").selectOptionByValue(String.valueOf(1995));
+        $(".react-datepicker__month").$(byText("17")).click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
         $("#hobbiesWrapper").$(byText("Reading")).click();
         $("#uploadPicture").uploadFromClasspath("cats.jpg");
@@ -29,10 +26,8 @@ public class TestForm extends TestBase {
         $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Agra")).click();
-
         $("#submit").click();
-
-        $(byClassName("modal-content")).shouldHave(text("Veronika"),
+        $(".modal-content").shouldHave(text("Veronika"),
                 text("Iatckaia"),
                 text("neznatnov@gmail.com"),
                 text("Female"), text("0123456789"),
@@ -42,10 +37,5 @@ public class TestForm extends TestBase {
                 text("cats.jpg"),
                 text("Street 1"),
                 text("Uttar Pradesh" + " " + "Agra"));
-
-
-
-
-
     }
 }
