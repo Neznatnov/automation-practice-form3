7ba3eb3538ed6ca87a291185d26081682965fa43
Veronika Iatckaia <neznatnov@gmail.com>
2023-04-23 20:58:07 +0200

    add pageobjects

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,121 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import components.CalendarComponent;
+import components.ResultsModal;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    ResultsModal resultsModal = new ResultsModal();
+
+
+    //Elements
+    SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            gender = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            subjectInput = $("#subjectsInput"),
+            hobbiesInput = $("#hobbiesWrapper"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            stateInput = $("#state"),
+            cityInput = $("#city");
+
+
+    //Actions
+    public RegistrationPage openPage(){
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        return this;
+    }
+    public RegistrationPage setFirstName(String value){
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+    public RegistrationPage setLastName(String value){
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+    public RegistrationPage setUserEmail(String value){
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+    public RegistrationPage setGender(String value){
+        gender.$(byText(value)).click();
+        return this;
+    }
+    public RegistrationPage setUserNumber(String value){
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+    public RegistrationPage setBirthDate(String day, String month, String year){
+        calendarComponent.setDate(day,month,year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubject(String value){
+        subjectInput.setValue(value).pressEnter();
+
+        return this;
+    }
+    public RegistrationPage setHobbies(String value){
+        hobbiesInput.$(byText(value)).click();
+
+        return this;
+    }
+    public RegistrationPage setPicture(){
+        uploadPicture.uploadFromClasspath("cats.jpg");
+
+        return this;
+    }
+    public RegistrationPage setAddress(String value){
+        currentAddressInput.setValue(value);
+
+        return this;
+    }
+    public RegistrationPage setState(String value){
+        stateInput.click();
+        stateInput.$(byText(value)).click();
+
+        return this;
+    }
+    public RegistrationPage setCity(String value){
+        cityInput.click();
+        cityInput.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage clickSubmit(){
+        $("#submit").click();
+        return this;
+    }
+
+    public RegistrationPage verifyRegistrationResultModalAppears(){
+        resultsModal.verifyModalAppears();
+
+        return this;
+    }
+
+
+
+    public RegistrationPage verifyResult(String key, String value){
+        resultsModal.verifyResult(key, value);
+
+        return this;
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,12 @@
+package tests;
+
 import com.codeborne.selenide.Configuration;
+import components.CalendarComponent;
 import org.junit.jupiter.api.BeforeAll;
+import pages.RegistrationPage;
+
 public class TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
     @BeforeAll
     static void BeforeAll(){
         Configuration.browser = "chrome";
